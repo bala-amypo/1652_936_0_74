9a39567f93dddd19dc2638636b7dd686c351374d
bala-amypo <bala@amypo.in>
2025-12-18 08:55:24 +0000

    User commit

--- src/main/java/com/example/demo/controller/ValidationController.java
+++ src/main/java/com/example/demo/controller/ValidationController.java
@@ -1,5 +1,9 @@
 package com.example.demo.controller;
 
+@Restcontroller
 public class ValidationController {
-    
+   @AutoWired ValidationService service;
+
+   @PostMapping("/po")
+   public ValidationEntity send()
 }
--- src/main/java/com/example/demo/service/Impl/ValidationServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/ValidationServiceImpl.java
@@ -2,5 +2,9 @@ package com.example.demo.service.Impl;
 @Service
 public class ValidationServiceImpl implements ValidationService {
     @Autowired ValidationRepository validation;
-     ValidationEntity postdata(@RequestBody ValidationEntity en);
+     
+     @Override
+     public ValidationEntity postdata(@RequestBody ValidationEntity en){
+         return validation.save(en);
+     }
 }
