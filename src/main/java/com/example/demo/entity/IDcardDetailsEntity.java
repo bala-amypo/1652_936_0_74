36e27bd08c3a03cb9b51f4c6f018c2744b5dd572
bala-amypo <bala@amypo.in>
2025-12-19 09:38:39 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/IDcardDetailsEntity.java
@@ -0,0 +1,14 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.util.Date;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+public class IDcardDetails {
+     
+}
