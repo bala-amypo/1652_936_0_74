5e852df26e40be62d7a34f03f6363cfec5bc938a
bala-amypo <bala@amypo.in>
2025-12-19 06:28:24 +0000

    User commit

--- src/main/java/com/example/demo/entity/ValidationEntity.java
+++ src/main/java/com/example/demo/entity/ValidationEntity.java
@@ -30,52 +30,6 @@ public class ValidationEntity {
    @Positive(message="Age must be positive")
    private Integer age;
 
-   public void setId(Long id){
-        this.id=id;
-       }
-       public Long getId(){
-        return id;
-       }
-       public String getUsername(){
-        return username;
-       }
-       public void setUsername(String username){
-        this.username=username;
-       }
-       public String getEmail(){
-        return email;
-       }
-       public void setEmail(String email){
-        this.email=email;
-       }
-       public String getPassword(){
-        return password;
-       }
-       public void setPassword(String password){
-        this.password=password;
-       }
-       public int getAge(){
-        return age;
-       }
-       public void setAge(int age){
-        this.age=age;
-       }
-
-       public ValidationEntity(Long id,
-       @NotNull @Size(min = 2,max = 10,message = "must be 2 to 10 character") String username,
-       @Email(message="Email is valid") String email,
-       @Size(min = 2,max = 8) @NotNull(message="Password is mandatory") String password,
-       @Max(30) @Positive(message="Age must be positive") int age){
-        this.id=id;
-        this.username=username;
-        this.email=email;
-        this.password=password;
-        this.age=age;
-        }
-        public ValidationEntity(){
-
-        }
-
-
+   
    
 }
