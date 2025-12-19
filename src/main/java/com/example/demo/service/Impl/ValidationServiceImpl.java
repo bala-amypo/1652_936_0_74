3f2dc77f4ae9ee0dc6edf34d4c94c382d7be863a
bala-amypo <bala@amypo.in>
2025-12-19 04:41:02 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/ValidationServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/ValidationServiceImpl.java
@@ -16,6 +16,6 @@ public class ValidationServiceImpl implements ValidationService {
      }
      @Override
      public ValidationEntity getdata(int id){
-        return validation.findById(id);
+        return validation.findById(id).orElse(other:null);
      }
 }
